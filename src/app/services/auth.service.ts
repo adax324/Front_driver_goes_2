5cf019e263d592484c61b1e73c816828aa75af44
Adaxator <adax324@gmail.com>
2023-03-19 11:15:52 +0100

    3

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,27 +9,33 @@ import {Router} from "@angular/router";
   providedIn: 'root'
 })
 export class AuthService {
-  private url = environment.apiUrl + '/user';
+  private url = environment.apiUrl + '/api/auth';
   authenticated = false;
 
   constructor(private http: HttpClient, private router:Router,) {
 
   }
-  authenticate(credentials:any, callback: any) {
-    let headers = new HttpHeaders();
-    if (credentials)
-      headers = headers.append("Authorization", 'Basic ' + btoa(credentials.username + ':' + credentials.password));
-    this.http.get<Response>(this.url, {
-      headers: headers,
-      observe: 'response',
-      withCredentials: true
-    }).subscribe(response => {
-      if (response && Object.values(response)[5])
-        this.authenticated = true;
-      else
-        this.authenticated = false;
-      return callback && callback();
-    })
+  // OLD BASIC AUTH
+  // authenticate(credentials:any, callback: any) {
+  //   let headers = new HttpHeaders();
+  //   if (credentials)
+  //     headers = headers.append("Authorization", 'Basic ' + btoa(credentials.username + ':' + credentials.password));
+  //   this.http.get<Response>(this.url, {
+  //     headers: headers,
+  //     observe: 'response',
+  //     withCredentials: true
+  //   }).subscribe(response => {
+  //     if (response && Object.values(response)[5])
+  //       this.authenticated = true;
+  //     else
+  //       this.authenticated = false;
+  //     return callback && callback();
+  //   })
+  // }
+
+  async authenticate(credentials: any, callback: any) {
+    let resposne = await this.http.post(this.url + '/signin', credentials).toPromise();
+    console.log(resposne);
   }
   logout() {
     this.http.post(environment.apiUrl + '/logout', {}).subscribe(
